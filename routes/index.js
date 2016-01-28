8f8e8f39230a3846cc9e9139ed5f6c383142afce
Hiraji <hiraji@compumatrice.com>
2016-01-28 15:47:05 +0530

    change made

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var every = require('every-moment');
 var router = express.Router();
 //var connection  = require('express-myconnection');
-router.set('port', (process.env.PORT || 5000));
+
 var  mysql = require('mysql');
 
    var  connectionObj=mysql.createConnection({
